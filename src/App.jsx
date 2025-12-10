c5b58160402aa2d31e538987d4fd721c2e0b2eb0
Kenny Panyavong <elp.kenny@gmail.com>
2025-12-09 19:54:40 -0600

    Create non-functional Login and Register components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Login } from "./components/auth/Login.jsx"
+import { Register } from "./components/auth/Register.jsx"
 import { ViewGame } from "./components/games/ViewGame.jsx"
-// import { ViewWishlist } from "./components/wishlists/ViewWishlist.jsx"
-// import { Wishlist } from "./components/wishlists/Wishlist.jsx"
+import { ViewWishlist } from "./components/wishlists/ViewWishlist.jsx"
+import { Wishlist } from "./components/wishlists/Wishlist.jsx"
 
 export const App = () => {
   return (
-    // <Wishlist />
-    // <ViewWishlist />
-    <ViewGame />
+    <>
+      <Login />
+      <Register />
+      <Wishlist />
+      <ViewWishlist />
+      <ViewGame />
+    </>
   )
 }
