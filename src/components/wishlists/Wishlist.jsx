9661c05cb52850d8410ba4ec51d72a0630da580a
Kenny Panyavong <elp.kenny@gmail.com>
2025-12-15 20:23:23 -0600

    Users now only view their wishlists

--- src/components/wishlists/Wishlist.jsx
+++ src/components/wishlists/Wishlist.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react"
-import { getAllWishlists } from "../../services/wishlistService.jsx"
+import { getWishlistsByUserId } from "../../services/wishlistService.jsx"
 import { Link } from "react-router-dom"
+import { getLoggedInUser } from "../auth/Auth.jsx"
 
 // define and export Wishlist function
 export const Wishlist = () => {
     const [wishlists, setWishlists] = useState([])
+    const loggedInUser = getLoggedInUser()
 // fetch wishlists from api
     useEffect(() => {
-        getAllWishlists().then((wishlistArray) => {
-            setWishlists(wishlistArray)
-        })
-    }, [])
+        getWishlistsByUserId(loggedInUser.id).then(setWishlists)
+    }, [loggedInUser.id])
 // map over the wishlists array and create an element for each wishlist
     return (
         <div>
